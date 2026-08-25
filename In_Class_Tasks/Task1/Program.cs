ef89c40818a796455028321d1c9cd9b67337df11
Nathan Wolfe <nw00030@mix.wvu.edu>
2026-08-25 13:12:09 -0400

    add Task1

---
+++ In_Class_Tasks/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Nathan
+ * Task1
+ * 08/25/2026
+ */
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
